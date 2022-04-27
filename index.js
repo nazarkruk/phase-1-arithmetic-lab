bd167624ff4a43b190319c57d37b585620d91d55
Nazar Kruk <nazarkruk@icloud.com>
2022-04-26 23:54:59 -0400

    commit tree d2502e9eb058cd31d29672d74dcbc3a91ef9d745

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*10) + 1;
+
+
+const num3 = 19;
+const num4  = 5;
+let mod = num3 % num4;
+
+const max = Math.max(2,4,6,20);
